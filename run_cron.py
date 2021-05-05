3bf44ad76034d9bb94ebf9ef093f6cc2af0329c6
prash29 <gillyprash29@gmail.com>
2021-05-05 15:29:39 +0530

    Updated files

--- run_cron.py
+++ run_cron.py
@@ -1,6 +1,7 @@
 from crontab import CronTab
 import os
 base_path = os.path.dirname(os.path.abspath(__file__))
+os.chmod(os.path.join(base_path,'main.py'), 0o775)
 
 cron = CronTab(user='prashant')
 job = cron.new(command='python {}/main.py >>  {}/cron.log 2>&1'.format(base_path, base_path))
